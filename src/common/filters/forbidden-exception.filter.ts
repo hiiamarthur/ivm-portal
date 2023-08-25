eaaf6f2973c9e9cc024a854fbf68067d8c995192
arthur.lau@ivmtech.com <arthur.lau@ivmtech.com>
2023-08-25 15:36:58 +0800

    add password control

--- src/common/filters/forbidden-exception.filter.ts
+++ src/common/filters/forbidden-exception.filter.ts
@@ -1,19 +1,19 @@
 import {
- ExceptionFilter,
- Catch,
- ArgumentsHost,
- HttpException,
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
 } from '@nestjs/common';
 import { Response } from 'express';
 import { ForbiddenException } from '@nestjs/common';
-   
+
 @Catch(ForbiddenException)
 export class ForbiddenExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  async catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
- 
+
     response.status(status).redirect('/login');
   }
 }
--- src/owner/owner.service.ts
+++ src/owner/owner.service.ts
@@ -13,25 +13,25 @@ import { Campaign } from '../entities/campaign';
 export class OwnerService extends IService {
 
     findAOwner = async (params: any) => {
-        const { loginId, password, schema } = params; 
+        const { loginId, password, schema } = params;
         const ds = await this.getEntityManager(schema);
-        
+
         try {
             const ownerLogin = await ds.getRepository(OwnerLogin).createQueryBuilder('login')
                 .leftJoinAndSelect('login.owner', 'owner')
                 .leftJoinAndSelect('owner.permissions', 'permissions')
                 .where('ON_Active = 1 and ONL_Login = :loginId COLLATE SQL_Latin1_General_CP1_CS_AS', { loginId: loginId })
                 .getOneOrFail();
-            if(password) {
+            if (password) {
                 const hashed = String(crypto.SHA512(password)).toLowerCase();
                 const checkPassword = ownerLogin.ONL_Password.toLowerCase() === hashed;
-                if(!checkPassword) {
+                if (!checkPassword) {
                     throw new UnauthorizedException('authentication fail');
                 }
             }
 
             return ownerLogin;
-        } catch(error) {
+        } catch (error) {
             throw new NotFoundException(`user ${loginId} not found`);
         }
     }
@@ -42,45 +42,45 @@ export class OwnerService extends IService {
         let whereClause = 'JSON_VALUE(owner.ON_ExtraData, \'$.Role\') is not null AND login.ONL_ExpireDate >= GETDATE()';
         let queryParameter = {};
 
-        if(username) {
+        if (username) {
             whereClause += ' AND ONL_OwnerID <> :username';
             queryParameter = { ...queryParameter, username: username };
         }
-        if(isActive !== undefined) {
+        if (isActive !== undefined) {
             whereClause += ' AND ONL_Active = :isActive AND owner.ON_Active = :isActive';
             queryParameter = { ...queryParameter, isActive: Number(isActive) };
         }
-        if(ownerId) {
+        if (ownerId) {
             whereClause += ' AND owner.ON_OwnerID like :ownerId';
-            queryParameter = {...queryParameter, ownerId: `%${ownerId}%` }
+            queryParameter = { ...queryParameter, ownerId: `%${ownerId}%` }
         }
-        if(ownerLogin) {
+        if (ownerLogin) {
             whereClause += ' AND ONL_Login like :ownerLogin';
             queryParameter = { ...queryParameter, ownerLogin: `%${ownerLogin}%` }
         }
-        if(ownerName) {
+        if (ownerName) {
             whereClause += ' AND (owner.ON_OwnerName like :ownerName OR owner.ON_OwnerNameEng like :ownerName)';
-            queryParameter = {...queryParameter, ownerName: `%${ownerName}%` }
+            queryParameter = { ...queryParameter, ownerName: `%${ownerName}%` }
         }
-        if(userRole) {
+        if (userRole) {
             whereClause += ' AND JSON_VALUE(owner.ON_ExtraData, \'$.Role\') = :userRole';
-            queryParameter = {...queryParameter, userRole: userRole }
+            queryParameter = { ...queryParameter, userRole: userRole }
         }
 
         const sStart = (Number.isNaN(parseInt('NaN')) || start === undefined) ? 0 : start;
-        
+
         const count = await ds.getRepository(OwnerLogin).createQueryBuilder('login')
             .select('count(distinct login.ONL_Login) as total')
             .leftJoin(Owner, 'owner', 'owner.ON_OwnerID = login.ONL_OwnerID')
             .where(whereClause, queryParameter)
             .getRawOne();
-                
-        if(!count) {
+
+        if (!count) {
             return datatableNoData;
         }
 
         let sLength = length;
-        if(Number.isNaN(parseInt(length)) || parseInt(length) <= 0 || length === undefined) {
+        if (Number.isNaN(parseInt(length)) || parseInt(length) <= 0 || length === undefined) {
             sLength = count.total;
         }
 
@@ -88,21 +88,21 @@ export class OwnerService extends IService {
             .createQueryBuilder('login')
             .leftJoinAndSelect('login.owner', 'owner')
             .where(whereClause, queryParameter);
-            
-        
-        if(sort && sort.length > 0) {
+
+
+        if (sort && sort.length > 0) {
             sort.forEach((s) => {
                 qb.addOrderBy(s.column, s.dir)
             })
         } else {
             qb.orderBy('owner.ON_Lastupdate', 'DESC');
         }
-        
+
         const data = await qb.offset(sStart).limit(sLength).getMany();
-       
+
         const rowData = data.map(d => {
-            const controls = `<a href="/owner/profile?loginId=${d.ONL_Login}" class="pe-3 action-icon"> <i class="fa fa-eye"></i></a>` + 
-            `<a href="javascript:void(0);" class="action-icon" data-bs-attrid="${d.ONL_Login}" data-bs-action="delete-user" data-bs-title="Delete User" data-bs-toggle="modal" data-bs-target="#confirmModal"> <i class="mdi mdi-delete"></i></a>`;
+            const controls = `<a href="/owner/profile?loginId=${d.ONL_Login}" class="pe-3 action-icon"> <i class="fa fa-eye"></i></a>` +
+                `<a href="javascript:void(0);" class="action-icon" data-bs-attrid="${d.ONL_Login}" data-bs-action="delete-user" data-bs-title="Delete User" data-bs-toggle="modal" data-bs-target="#confirmModal"> <i class="mdi mdi-delete"></i></a>`;
             return {
                 id: d.owner.ON_OwnerID,
                 loginName: d.ONL_Login,
@@ -115,7 +115,7 @@ export class OwnerService extends IService {
                 control: controls
             }
         })
-         return { 
+        return {
             page: sStart,
             recordsTotal: count?.total || 0,
             recordsFiltered: count?.total || 0,
@@ -127,10 +127,10 @@ export class OwnerService extends IService {
         const { ownerId, schema } = params;
         const ds = await this.getEntityManager(schema);
         const qb = ds.getRepository(Machine).createQueryBuilder('machine')
-                .leftJoinAndSelect('machine.type', 'type')
-                .where('M_Active = 1 and M_Name not like :name', { name: `%收機%` })
-                .orderBy('M_MachineID');
-        if(ownerId) {
+            .leftJoinAndSelect('machine.type', 'type')
+            .where('M_Active = 1 and M_Name not like :name', { name: `%收機%` })
+            .orderBy('M_MachineID');
+        if (ownerId) {
             return await qb.andWhere('M_MachineID in (select ONM_MachineID from Owner_Machine where ONM_OwnerID = :ownerId)', { ownerId: ownerId }).getMany();
         } else {
             return await qb.getMany();
@@ -141,16 +141,16 @@ export class OwnerService extends IService {
         const { ownerId, schema } = params;
         const ds = await this.getEntityManager(schema);
         const qb = ds.getRepository(Product).createQueryBuilder('products')
-                    .leftJoinAndSelect('products.category', 'category')
-                    .leftJoinAndSelect('products.detail', 'detail')
-                    .where('MP_Active = 1 AND MP_ExtraData <> \'\'')
-                    .orderBy('MP_ProductID');
-        if(ownerId) {
+            .leftJoinAndSelect('products.category', 'category')
+            .leftJoinAndSelect('products.detail', 'detail')
+            .where('MP_Active = 1 AND MP_ExtraData <> \'\'')
+            .orderBy('MP_ProductID');
+        if (ownerId) {
             //const oProducts = await this.entityManager.query('select ONPL_ProductID from Owner_ProductList where ONPL_OwnerID = @0', [ownerId]);
             //return await qb.andWhere('MP_ProductID in (:...productIds)', { productIds: oProducts.map(p => p.ONPL_ProductID)}).getMany();
             return await ds.getRepository(MachineProduct).createQueryBuilder()
-            .select(['MP_ProductID','MP_ProductName', 'MP_UnitPrice'])
-            .where('MP_MachineID in (select ONM_MachineID from Owner_Machine where ONM_OwnerID = :ownerId)', { ownerId: ownerId }).getRawMany();
+                .select(['MP_ProductID', 'MP_ProductName', 'MP_UnitPrice'])
+                .where('MP_MachineID in (select ONM_MachineID from Owner_Machine where ONM_OwnerID = :ownerId)', { ownerId: ownerId }).getRawMany();
         } else {
             return await qb.getMany();
         }
@@ -160,10 +160,10 @@ export class OwnerService extends IService {
         const { ownerId, schema } = params;
         const ds = await this.getEntityManager(schema);
         const qb = ds.getRepository(Stock).createQueryBuilder('stocks')
-        .leftJoinAndSelect('stocks.category', 'category')
-        .where('MS_Active = 1 AND MS_ExtraData <> \'{}\'')
-        .orderBy('MS_StockCode');
-        if(ownerId) {
+            .leftJoinAndSelect('stocks.category', 'category')
+            .where('MS_Active = 1 AND MS_ExtraData <> \'{}\'')
+            .orderBy('MS_StockCode');
+        if (ownerId) {
             //const oSkus = await this.entityManager.query('select ONSL_StockCode from Owner_StockList where ONSL_OwnerID = @0', [ownerId]);
             //return await qb.andWhere('MS_StockCode in (:...stockCodes)', { stockCodes: oSkus.map(s => s.ONSL_StockCode )}).getMany();
             return await ds.getRepository(MachineStock).createQueryBuilder().where('MS_MachineID in (select ONM_MachineID from Owner_Machine where ONM_OwnerID = :ownerId)', { ownerId: ownerId }).getMany();
@@ -171,36 +171,36 @@ export class OwnerService extends IService {
             return await qb.getMany();
         }
     }
-    
+
     getOwnerCampaigns = async (params: any) => {
         const { ownerId, schema } = params;
         const ds = await this.getEntityManager(schema);
-        if(ownerId) {
+        if (ownerId) {
             return await ds.getRepository(Campaign).createQueryBuilder('campaign')
-            .where('RC_CampaignID IN (select ONC_CampaignID from Owner_Campaign where ONC_OwnerID = :ownerId)', { ownerId: ownerId }).getMany();
+                .where('RC_CampaignID IN (select ONC_CampaignID from Owner_Campaign where ONC_OwnerID = :ownerId)', { ownerId: ownerId }).getMany();
         } else {
             return await ds.getRepository(Campaign).createQueryBuilder().where('RC_Active = 1').getMany();
         }
-        
+
     }
 
     insertLoginLog = async (ownerId: string, address: string, success: boolean, schema: string) => {
         try {
             const ds = await this.getEntityManager(schema);
             const result = await ds.createQueryBuilder()
-            .insert()
-            .into(LoginLog)
-            .values({
-                action: `${ownerId} login [${schema}]`,
-                statusCode: success ? 200 : 403,
-                loginId: ownerId,
-                detail: success ? 'Success' : 'Fail',
-                ipAddress: address
-            })
-            .returning('INSERTED.*')
-            .execute();
+                .insert()
+                .into(LoginLog)
+                .values({
+                    action: `${ownerId} login [${schema}]`,
+                    statusCode: success ? 200 : 403,
+                    loginId: ownerId,
+                    detail: success ? 'Success' : 'Fail',
+                    ipAddress: address
+                })
+                .returning('INSERTED.*')
+                .execute();
             return result;
-        } catch(error) {
+        } catch (error) {
             return null
         }
     }
@@ -226,9 +226,9 @@ export class OwnerService extends IService {
         const { ON_OwnerID, ONL_Login, ONL_Password, ONL_ExpireDate, userRole } = owner;
         const ds = await this.getEntityManager(schema);
         const entity = owner;
-        
-        if(userRole) {
-            entity.ON_ExtraData = { Role: userRole, Type: userRole === 'SuperAdmin' ? 'Admin': 'User', FirstLogin: 0 }
+
+        if (userRole) {
+            entity.ON_ExtraData = { Role: userRole, Type: userRole === 'SuperAdmin' ? 'Admin' : 'User', FirstLogin: 0 }
         }
 
         let ownerLogin = await ds.getRepository(OwnerLogin).findOne({
@@ -236,39 +236,39 @@ export class OwnerService extends IService {
                 ONL_Login: ONL_Login
             }
         })
-        if(!ownerLogin){
+        if (!ownerLogin) {
             ownerLogin = new OwnerLogin();
             ownerLogin.ONL_Active = true;
             ownerLogin.ONL_OwnerID = ON_OwnerID;
             ownerLogin.ONL_Login = ONL_Login;
         }
-        if(ONL_Password) {
+        if (ONL_Password) {
             ownerLogin.ONL_Password = String(crypto.SHA512(ONL_Password));
         }
-        if(ONL_ExpireDate) {
+        if (ONL_ExpireDate) {
             ownerLogin.ONL_ExpireDate = parse(`${ONL_ExpireDate} 23:59:00`, 'yyyy-MM-dd HH:mm:ss', new Date());
-        } else { 
+        } else {
             ownerLogin.ONL_ExpireDate = addYears(endOfDay(new Date()), 1);
         }
-        
+
         entity.ON_Lastupdate = new Date();
-        if(machineIds) {
+        if (machineIds) {
             const machines = await ds.getRepository(Machine).createQueryBuilder()
                 .where('M_MachineID in (:...machineIds)', { machineIds: machineIds })
                 .getMany();
-                entity.machines = machines;
+            entity.machines = machines;
         } else {
             entity.machines = null;
         }
-        if(campaignIds) {
+        if (campaignIds) {
             const campaigns = await ds.getRepository(Campaign).createQueryBuilder()
                 .where('RC_CampaignID in (:...campaignIds)', { campaignIds: campaignIds })
                 .getMany();
-                entity.campaigns = campaigns;
+            entity.campaigns = campaigns;
         } else {
             entity.campaigns = null;
         }
-        
+
         try {
             await ds.getRepository(OwnerLogin).save(ownerLogin);
             await ds.getRepository(Owner).save(entity);
@@ -289,7 +289,7 @@ export class OwnerService extends IService {
     }
 
     changePassword = async (params: any) => {
-        const { loginId, oldPassword, newPassword, schema } = params;
+        const { loginId, oldPassword, newPassword, schema, isSuperAdmin } = params;
         const ds = await this.getEntityManager(schema);
         try {
             const ownerLogin = await ds.getRepository(OwnerLogin).findOneOrFail({
@@ -297,10 +297,12 @@ export class OwnerService extends IService {
                     ONL_Login: loginId
                 }
             })
-            const hashed = String(crypto.SHA512(oldPassword)).toLowerCase();
-            const checkPassword = ownerLogin.ONL_Password.toLowerCase() === hashed;
-            if(!checkPassword) {
-                throw new BadRequestException('wrong old password');
+            if(!isSuperAdmin){
+                const hashed = String(crypto.SHA512(oldPassword)).toLowerCase();
+                const checkPassword = ownerLogin.ONL_Password.toLowerCase() === hashed;
+                if (!checkPassword) {
+                    throw new BadRequestException('wrong old password');
+                }
             }
             const newHashed = String(crypto.SHA512(newPassword)).toLowerCase();
             ownerLogin.ONL_Password = newHashed;
@@ -325,7 +327,7 @@ export class OwnerService extends IService {
             entity.owner.permissions = null;
             entity.owner.machines = null;
             await ds.getRepository(OwnerLogin).save(entity);
-        } catch(error) {
+        } catch (error) {
             throw error;
         }
     }
